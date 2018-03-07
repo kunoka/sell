183303dc2f42d9f4fa478b205691e6b7f30a5330
Harry <hengwang@cn.ibm.com>
2018-03-07 10:08:28 +0800

    chapter 5-3 end

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!--<img src="./assets/logo.png">-->
+    <Header></Header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+    <div>I am content</div>
     <!--<Hello></Hello>-->
   </div>
 </template>
 
 <script>
-import Hello from './components/HelloWorld.vue'
+import Header from './components/header/header.vue'
 
 export default {
   name: 'App',
   components: {
-    Hello: Hello
+    Header: Header
   }
 }
 </script>
 
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display:flex
+      width: 100%
+      height 40px
+      line-height: 40px
+      .tab-item
+        flex: 1
+        text-align: center
 </style>
