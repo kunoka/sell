88592e920e1c90b6a3cc99df33450a06801e4eb7
Harry <hengwang@cn.ibm.com>
2018-03-08 17:31:11 +0800

    chapter 5-6 1 end

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,25 @@
     <!--<img src="./assets/logo.png">-->
     <Header></Header>
     <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评论</div>
-      <div class="tab-item">商家</div>
+      <div class="tab-item">
+        <router-link to='/goods'>商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to='/ratings'>评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to='/seller'>商家</router-link>
+      </div>
     </div>
-    <div>I am content</div>
-    <!--<Hello></Hello>-->
+    <!--路由外链-->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import Header from './components/header/header.vue'
-
 export default {
-  name: 'App',
+  name: 'app',
   components: {
     Header: Header
   }
@@ -24,13 +29,20 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  #app
     .tab
       display:flex
       width: 100%
       height 40px
       line-height: 40px
+      border: 1px solid rgba(7,17,27,0.1)
       .tab-item
         flex: 1
         text-align: center
+        & > a
+          display: block
+          font-size 14px
+          background-color: yellow
+          color: rgb(77, 85, 93)
+          &.active
+            color: blue
 </style>
