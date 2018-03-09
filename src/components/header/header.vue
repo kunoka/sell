60ede589c3a0e158f4db7f41028ad7b750db5a5f
Harry <hengwang@cn.ibm.com>
2018-03-09 10:11:14 +0800

    chapter 6-08 end

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -22,7 +22,14 @@
         </div>
       </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" alt="" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
@@ -41,9 +48,11 @@ export default {
 
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl"
+  /*@import "../../common/stylus/icon.styl"*/
   .header
+    position relative
     color: #fff
-    background: #999
+    background rgba(7, 17, 27, 0.5)
     .content-wrapper
       position relative
       padding: 24px 12px 18px 24px
@@ -116,4 +125,39 @@ export default {
           margin-left 2px
           line-height 24px
           font-size 10px
+    .bulletin-wrapper
+      position relative
+      height 28px
+      line-height 28px
+      padding 0 22px 0 12px
+      white-space nowrap
+      overflow hidden
+      text-overflow ellipsis
+      background rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display inline-block
+        vertical-align top
+        margin-top 7px
+        width 22px
+        height 12px
+        bg-image('bulletin')
+        background-size 22px 12px
+        background-repeat no-repeat
+      .bulletin-text
+        vertical-align top
+        margin 0 4px
+        font-size 10px
+      .icon-keyboard_arrow_right
+        position absolute
+        font-size 10px
+        right 12px
+        top 8px
+    .background
+      position absolute
+      top 0
+      left 0
+      width 100%
+      height 100%
+      z-index -1
+      filter blur(10px)
 </style>
