9189d973f795f26306e7106d6917ddc7714f0d27
Harry <hengwang@cn.ibm.com>
2018-03-12 14:09:54 +0800

    chapter 6-13 end

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -7,7 +7,7 @@
       <div class="content">
         <div v-if="seller.supports" class="support-count" @click="showDetail">
           <span class="count">{{seller.supports.length}}个</span>
-          <i class="icon-keyboard_arrow_right"></i>
+          <i class="icon-keyboard_arrow_right">></i>
         </div>
         <div class="title">
           <span class="brand"></span>
@@ -30,11 +30,25 @@
     <div class="background">
       <img :src="seller.avatar" alt="" width="100%" height="100%">
     </div>
-    <div v-show="detailShow" class="detail"></div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+          <div class="star-wrapper">
+            <star :size="48" :score="4.5"></star>
+          </div>
+
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close">X</i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import star from '../star/star.vue'
 export default {
   props: {
     seller: {
@@ -53,13 +67,16 @@ export default {
   },
   created() {
     this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+  },
+  components: {
+    star: star
   }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl"
-  /*@import "../../common/stylus/icon.styl"*/
+  @import "../../common/stylus/base.styl"
   .header
     position relative
     overflow hidden
@@ -128,7 +145,7 @@ export default {
         height 24px
         line-height 24px
         border-radius 14px
-        background rgba(0,0,0,0.2)
+        background rgba(0,0,0,1)
         text-align center
         .count
           vertical-align top
@@ -172,7 +189,7 @@ export default {
       height 100%
       z-index -1
       filter blur(10px)
-    .detail {
+    .detail
       position fixed
       top: 0
       left: 0
@@ -180,5 +197,25 @@ export default {
       width 100%
       background rgba(7, 17, 27, 0.8)
       z-index: 100
-    }
+      .detail-wrapper
+        width 100%
+        min-height 100%
+        .detail-main
+          margin-top 64px
+          padding-bottom 64px
+          .name
+            line-height 16px
+            text-align center
+            font-size 16px
+            font-weight 700
+          .star-wrapper
+            margin-top 18px
+            padding 2px 0px
+            text-align center
+      .detail-close
+        position relative
+        width 32px
+        height 32px
+        margin -64px auto
+        clear: both
 </style>
