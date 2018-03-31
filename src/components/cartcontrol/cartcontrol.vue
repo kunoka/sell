33882d2dae447842c7738dccabf3eab8aae5804f
Harry <hengwang@cn.ibm.com>
2018-03-31 10:43:23 +0800

    7-16 cartcontrol组件（2）end

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="cartcontrol">
-  <div class="cart-decrease"></div>
+  <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreaseCart"></div>
   <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
   <div class="cart-add icon-add_circle" @click="addCart">
   </div>
@@ -8,6 +8,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+import Vue from 'vue'
 export default {
   props: {
     food: {
@@ -15,8 +16,25 @@ export default {
     }
   },
   methods: {
-    addCart () {
+    addCart (event) {
+      if (!event._constructed) {
+        return
+      }
       console.log('addCart')
+      if (!this.food.count) {
+        Vue.set(this.food, 'count', 1)
+        this.food.count = 1
+      } else {
+        this.food.count++
+      }
+    },
+    decreaseCart (event) {
+      if (!event._constructed) {
+        return
+      }
+      if (this.food.count) {
+        this.food.count--
+      }
     }
   }
 }
@@ -33,4 +51,11 @@ export default {
       color rgb(0,160,220)
     .cart-count
       display inline-block
+      vertical-align top
+      width 12px
+      padding-top 6px
+      line-height 24px
+      text-align center
+      font-size 10px
+      color rgb(147,153,159)
 </style>
