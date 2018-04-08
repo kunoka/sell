f18213718c1123052d7fbf3a0a5275fd1e316d7a
Harry <hengwang@cn.ibm.com>
2018-04-08 08:52:28 +0800

    8-03 商品详情页实现（3）end

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -4,6 +4,20 @@
       <div class="food-content">
         <div class="image-header">
           <img :src="food.image" alt="">
+          <div @click="hide" class="back">
+            <i class="icon-arrow_lift"></i>
+          </div>
+        </div>
+        <div class="content">
+          <div class="name">{{food.name}}</div>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span>好评率{{food.rating}}</span>
+          </div>
+          <div class="price">
+            <span class="now">￥{{food.price}}</span>
+            <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -25,6 +39,9 @@ export default {
   methods: {
     _show () {
       this.showFlag = true
+    },
+    hide () {
+      this.showFlag = false
     }
   }
 }
@@ -39,9 +56,30 @@ export default {
     z-index 30
     width 100%
     background #fff
-    &.food-enter-active,.food-leave-active
+    &.food-enter-active,&.food-leave-active
       transition all 0.2s linear
       transform translate3d(0,0,0)
     &.food-enter,&.food-leave-to
       transform translate3d(100%,0,0)
+    .image-header
+      position relative
+      width 100%
+      height 0
+      padding-top 100%
+      /* padding-top是根据width计算的 */
+      img
+        position absolute
+        top 0
+        left 0
+        width 100%
+        height 100%
+    .back
+      position absolute
+      top 10px
+      left 0
+      .icon-arrow_lift
+        display block
+        font-size 20px
+        padding 10px
+        color #fff
 </style>
