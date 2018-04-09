c9182ee95f1f7630983f0d50ae4043fa8ee79e71
Harry <hengwang@cn.ibm.com>
2018-04-09 23:09:22 +0800

    8-09 ratingselect组件（3）end

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="ratingselect border-1px">
     <div class="rating-type">
-      <span>{{desc.all}}</span>
-      <span>{{desc.positive}}</span>
-      <span>{{desc.negative}}</span>
+      <span class="block positive" :class="{'active':selectType===2}">{{desc.all}}<span class="count">47</span></span>
+      <span class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span class="count">40</span></span>
+      <span class="block negative" :class="{'active':selectType===1}">{{desc.negative}}<span class="count">7</span></span>
     </div>
     <div class="switch">
       <span class="icon-check_circle"></span>
@@ -55,4 +55,27 @@ export default {
       padding 18px 0
       margin 0 18px
       border-1px(rgba(7,17,27,0.1))
+      .block
+        display inline-block
+        padding 8px 12px
+        margin-right 8px
+        line-height 16px
+        border-radius 1px
+        font-size 12px
+        color rgb(77,85,93)
+        margin-right 8px
+        &.active
+          color #fff
+        .count
+          margin-left 2px
+          font-size 8px
+        &.positive
+          background rgba(0,160,220,0.2)
+          &.active
+            background rgb(0,160,220,0)
+        &.negative
+          background rgba(77,85,93,0.2)
+          &.active
+            background rgb(77,85,93)
+
 </style>
