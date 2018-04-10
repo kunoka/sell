db852b3e46e9e4b5c9cbff9748ed450f04db9395
Harry <hengwang@cn.ibm.com>
2018-04-10 22:37:55 +0800

    8-12 评价列表（1）end

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -29,6 +29,22 @@
           <h1 class="title">商品评价</h1>
           <ratingselect :initial-select-type="selectType" :desc="desc" :initial-only-content="onlyContent" :ratings="food.ratings"></ratingselect>
         </div>
+        <div class="rating-wrapper">
+          <ul v-show="food.ratings && food.ratings.length">
+            <li class="rating-item" v-bind:key="rating.username" v-for="rating in food.ratings">
+              <div class="user">
+                <span class="username">{{rating.username}}</span>
+                <img class="avatar" width="12" height="12" :src="rating.avatar" alt="">
+              </div>
+              <div class="time">{{rating.rateTime}}</div>
+              <p class="text">
+                <span :class="{'icon-thumb_up': rating.rateType===0, 'icon-thumb_down': rating.rateType===1}"></span>
+                {{rating.text}}
+              </p>
+            </li>
+          </ul>
+          <div class="no-rating" v-show="!food.ratings || !food.ratings.length"></div>
+        </div>
       </div>
     </div>
   </transition>
