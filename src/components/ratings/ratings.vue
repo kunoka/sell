9281415d608c34248f4160ce4df67e29def1b726
Harry <hengwang@cn.ibm.com>
2018-04-13 10:00:14 +0800

    9-2 ratings组件开发-overview开发（2）

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -7,18 +7,41 @@
         <div class="title">统合评分</div>
         <div class="rank">高于周边商家{{seller.rankRate}}%</div>
       </div>
-      <div class="overview-right"></div>
+      <div class="overview-right">
+        <div class="score-wrapper">
+          <span class="title">服务态度</span>
+          <star class="star" :size=36 :score="seller.serviceScore"></star>
+          <span class="score">{{seller.serviceScore}}</span>
+        </div>
+        <div class="score-wrapper">
+          <span class="title">食物评分</span>
+          <star class="star" :size=36 :score="seller.foodScore"></star>
+          <span class="score">{{seller.foodScore}}</span>
+        </div>
+        <div class="deliver-wrapper">
+          <span class="title">送达时间</span>
+          <span class="delivery">{{seller.deliveryTime}}分钟</span>
+        </div>
+      </div>
     </div>
   </div>
 </div>
 </template>
 
 <script type="text/ecmascript-6">
+import star from 'components/star/star'
 export default {
   props: {
     seller: {
       type: Object
     }
+  },
+  created () {
+    console.log('seller')
+    console.log(this.seller)
+  },
+  components: {
+    star
   }
 }
 </script>
@@ -56,4 +79,32 @@ export default {
     .overview-right
       flex 1
       padding-left 24px
+      .score-wrapper
+        margin-bottom 8px
+        font-size 0
+        .title
+          display: inline-block
+          font-size 12px
+          line-height 18px
+          vertical-align top
+          color rgb(7,17,27)
+        .star
+          display inline-block
+          vertical-align top
+          margin 0 12px
+        .score
+          display inline-block
+          vertical-align top
+          line-height 18px
+          font-size 12px
+          color rgb(255,153,0)
+      .deliver-wrapper
+        font-size 0
+        .title
+          font-size 12px
+          color rgb(7,17,27)
+        .delivery
+          margin-left 12px
+          font-size 12px
+          color rgb(147,153,159)
 </style>
