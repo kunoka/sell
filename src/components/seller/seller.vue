00a12b22940ebabb0583871f8ff2e17d85f9fae7
Harry <hengwang@cn.ibm.com>
2018-04-17 22:57:12 +0800

    10-02 seller组件开发-overview开发（2）end

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -5,7 +5,7 @@
         <h1 class="title">{{seller.name}}</h1>
         <div class="desc">
           <star :size="36" :score="seller.score"></star>
-          <span class="text">{{seller.ratingCount}}</span>
+          <span class="text">({{seller.ratingCount}})</span>
           <span class="text">月售{{seller.sellCount}}单</span>
         </div>
         <ul class="remark">
@@ -29,13 +29,13 @@
           </li>
         </ul>
       </div>
+      <split></split>
     </div>
   </div>
 </template>
-
 <script type="text/ecmascript-6">
 import star from '../../components/star/star.vue'
-
+import split from '../../components/split/split'
 export default {
   props: {
     seller: {
@@ -43,11 +43,62 @@ export default {
     }
   },
   components: {
-    star
+    star,
+    split
   }
 }
 </script>
-
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+  .seller
+    position absolute
+    top 174px
+    bottom 0
+    left 0
+    width 100%
+    overflow hidden
+    .overview
+      padding 18px
+      .title
+        font-size 14px
+        line-height 14px
+        color rgb(7,17,27)
+        margin-bottom 8px
+      .desc
+        padding-bottom 18px
+        height 18px
+        border-1px(rgba(7,17,27,0.1))
+        .star
+          display inline-block
+          margin-right 8px
+          vertical-align top
+        .text
+          display inline-block
+          font-size 10px
+          line-height 18px
+          color rgb(77,85,93)
+          vertical-align top
+          margin-right 12px
+      .remark
+        display flex
+        padding 18px 0 0 0
+        .block
+          flex 1
+          text-align center
+          border-right 1px solid rgba(7,17,27,0.1)
+          &:last-child
+            border none
+          h2
+            font-size  10px
+            line-height 10px
+            color rgb(147,153,159)
+            margin-bottom 4px
+          .content
+            font-size 10px
+            font-weight 200
+            line-height 24px
+            color rgb(7,17,27)
+            .stress
+              font-size 24px
 
 </style>
